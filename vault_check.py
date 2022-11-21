c0f389c8a644054e100880aabf8910cec67dbd99
JimmyMo <jocund_mo@aliyun.com>
2022-11-21 10:03:50 +0800

    valult_check

--- vault_check.py
+++ vault_check.py
@@ -6,20 +6,38 @@ htmls_count = 0
 all_count = 0
 files_not_html = list()
 htmls_not_valid = list()
+duplicated_index = list()
+
+vault_index = dict()
 
 for dirpath, dirnames, filenames in os.walk(startDir):
     for filename in filenames:
         relative_path = dirpath.replace(startDir, "")  # strip the leading path
+
         all_count += 1
         if filename.endswith(".html"):
+            full_path = os.path.join(dirpath, filename).replace("\\", "/")
             htmls_count += 1
             print(filename)
             factors = filename.split("_")
+
+            # health check
+            is_health = True
             if len(factors) != 2:
+                is_health = False
                 if len(factors) > 1 and factors[1].strip() == "":
                     htmls_not_valid.append((filename, "索引号为空"))
                 else:
                     htmls_not_valid.append((filename, "超过或少于两个_"))
+
+            # make index
+            if is_health:
+                html_title = factors[0]
+                html_index = factors[1]
+                if html_index in vault_index:
+                    duplicated_index.append(filename)
+                else:
+                    vault_index[html_index] = full_path
         else:
             files_not_html.append(filename)
 
