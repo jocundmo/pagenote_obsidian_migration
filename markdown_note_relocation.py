159e159511370a723d791a89edc901dbe0d0c5ee
JimmyMo <jocund_mo@aliyun.com>
2022-11-22 10:03:47 +0800

    walk through the entire note vault

--- markdown_note_relocation.py
+++ markdown_note_relocation.py
@@ -5,8 +5,9 @@ import re
 from urllib.parse import unquote, quote
 
 vault_index_path = "vault_index.txt"
+startDir = "E:/ObsidianVault/Personal_testing"
 
-
+ignored_folders = [".git", ".obsidian", ".trash"]
 # from urllib.parse import urlencode
 # params2 = {
 #     'name': "王二",
@@ -42,27 +43,38 @@ def fregex(pattern, text, index):
 with codecs.open(vault_index_path, mode='r', encoding='utf-8') as f:
     vault_index = json.load(f)
 
-note_path = "小米手机MIX2解锁刷机.md"
-with codecs.open(note_path, mode='r', encoding='utf-8') as f:
-    content = f.read()
-    vault_links = fregex(r"\[本地知识库\]\(file:///(.*\.html)\)", content, [0, 1])
-    # vault_path = fregex(r"\[本地知识库\]\(file:///(.*\.html)\)", content, 1)
-    # vault_links_zipped = list(zip(vault_links, vault_path))
-    for link in vault_links:
-        index = link[1].split("_")[-1].rstrip(".html")
-        target_path = vault_index[index]
-        dir = "/".join(target_path.split("/")[0:-1])
-        filename = target_path.split("/")[-1]
-        encoded_filename = encode_min(filename)  # 这里已优化成只encode敏感字符，不处理中文
-        encoded_target_path = os.path.join(dir, encoded_filename).replace("\\", "/")
-        target_link = f"[本地知识库](file:///{encoded_target_path})"
-        content = content.replace(link[0], target_link)
+for dirpath, dirnames, filenames in os.walk(startDir):
+    dirnames[:] = list(set(dirnames) - set(ignored_folders))
+    for filename in filenames:
+        relative_path = dirpath.replace(startDir, "")  # strip the leading path
+        if filename.endswith(".md"):
+            note_path = os.path.join(dirpath, filename).replace("\\", "/")
+            # note_path = "小米手机MIX2解锁刷机.md"
+            with codecs.open(note_path, mode='r', encoding='utf-8') as f:
+                content = f.read()
+                vault_links = fregex(r"\[本地知识库\]\(file:///(.*\.html)\)", content, [0, 1])
 
-with codecs.open("modified_" + note_path, mode="w", encoding="utf-8") as f:
-    # output = json.dumps(content, ensure_ascii=False)
-    f.write(content)
+                modified = False
+                for link in vault_links:
+                    index = link[1].split("_")[-1].rstrip(".html")
+                    if index not in vault_index:
+                        continue
+                    target_path = vault_index[index]
+                    dir = "/".join(target_path.split("/")[0:-1])
+                    filename = target_path.split("/")[-1]
+                    encoded_filename = encode_min(filename)  # 这里已优化成只encode敏感字符，不处理中文
+                    encoded_target_path = os.path.join(dir, encoded_filename).replace("\\", "/")
+                    target_link = f"[本地知识库](file:///{encoded_target_path})"
+                    content = content.replace(link[0], target_link)
+                    modified = True
+                if modified:
+                    dir_name = os.path.dirname(note_path)
+                    f_name = note_path[len(dir_name)+1:]
+                    f_name_no_ext = f_name.rsplit(".")[0]
+                    ext_name = f_name.rsplit(".")[1] if len(f_name.rsplit(".")) >= 2 else None
 
-    # clean_content = unquote(content)
-    # print(clean_content)
-    # page_note_main = json.loads(clean_content)
-    # page_note_list = page_note_main["pages"]
+                    f_name_no_ext += "2"
+                    file_path_to_write = os.path.join(dir_name, f"{f_name_no_ext}.{ext_name}").replace("\\", "/")
+                    with codecs.open(file_path_to_write, mode="w", encoding="utf-8") as f:
+                        # output = json.dumps(content, ensure_ascii=False)
+                        f.write(content)
